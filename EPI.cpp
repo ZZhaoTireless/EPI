ad542d7c8b876f852a71a55a507d80638bad288e
ZZhaoTireless <zixu.zhao.tireless@gmail.com>
2017-02-07 21:10:40 -0500

    complete 6.3
    std::deque

--- EPI.cpp
+++ EPI.cpp
@@ -8,18 +8,18 @@
 
 /* Brute-force */
 
-int parityBruteForce (unsigned long num){
+int parityBruteForce (unsigned long num) {
 
 	short parity = 0;
-	while(num){
-    	
-		parity ^= (num & 1); 
+	while (num) {
+
+		parity ^= (num & 1);
 		num >>= 1;
 	}
 	return parity;
 }
 
-/* 
+/*
 Improve best and average case
 
 x & (x - 1) -> each time eliminte the lowest set bit
@@ -29,11 +29,11 @@ x & ~(x - 1) -> each time extract the lowest set bit
 lowest set bit -> first bit of 1 from most right position
 */
 
-int parityOnlyViewSetBit(unsigned long num){
+int parityOnlyViewSetBit(unsigned long num) {
 
 	short parity = 0;
 
-	while (num){
+	while (num) {
 		parity ^= 1;
 		num &= (num - 1);
 	}
@@ -49,22 +49,22 @@ XOR between the top half and the bottom half of a number keeps
 the parity of 1's and 0's for the whole number.
 */
 
-int parityDivAndConq(unsigned long long num){
+int parityDivAndConq(unsigned long long num) {
 
 	int shift = 32;
-	do{
+	do {
 		/*Show binary form of 'num'*/
-    	std::string binary = std::bitset<64>(num).to_string();
-    	std::cout << binary << std::endl;
+		std::string binary = std::bitset<64>(num).to_string();
+		std::cout << binary << std::endl;
 
 		num ^= (num >> shift);
 
-		/* This equls to 
+		/* This equls to
 
-		num ^= num >> 32;  
-			-> 0x00000000XXXXXXXX ^ 0xXXXXXXXXYYYYYYYY 
+		num ^= num >> 32;
+			-> 0x00000000XXXXXXXX ^ 0xXXXXXXXXYYYYYYYY
 			-> 0xXXXXXXXX ^ 0xYYYYYYYY
-		
+
 		num ^= num >> 16;
 		num ^= num >> 8;
 		num ^= num >> 4;
@@ -72,7 +72,7 @@ int parityDivAndConq(unsigned long long num){
 		num ^= num >> 1; */
 		shift >>= 1;
 
-	} while(shift);
+	} while (shift);
 
 	return num & 1;
 }
@@ -83,34 +83,34 @@ int parityDivAndConq(unsigned long long num){
 
 /* Brute-force */
 
-int swapBruteForce (unsigned long num, int i, int j){
+int swapBruteForce (unsigned long num, int i, int j) {
 
 	if (i == j) return num;
 
 	unsigned long maskOfI = (num >> i) & 0x1;
 	unsigned long maskOfJ = (num >> j) & 0x1;
 
-    if(maskOfI) num = num | (maskOfI << j);
-    else num = num & ~(0x1 << j);
+	if (maskOfI) num = num | (maskOfI << j);
+	else num = num & ~(0x1 << j);
 
-    if(maskOfJ) num = num | (maskOfJ << i);
-    else num = num & ~(0x1 << i);
+	if (maskOfJ) num = num | (maskOfJ << i);
+	else num = num & ~(0x1 << i);
 
-	return num;	
+	return num;
 }
 
 /*
-Improvement without save the value. 
+Improvement without save the value.
 
-Note: the "exclusive or" with 1 swap the bit value 
+Note: the "exclusive or" with 1 swap the bit value
 when values on i and j are different.
 
 1 ^ 1 = 0, 0 ^ 1 = 1
 */
 
-int swapByMask( unsigned long num, int i, int j){
+int swapByMask( unsigned long num, int i, int j) {
 
-	if(((num >> i) & 0x1) != ((num >> j) & 0x1)){
+	if (((num >> i) & 0x1) != ((num >> j) & 0x1)) {
 		unsigned int mask = (1L << i) | (1L << j);
 
 		num ^= mask;
@@ -127,17 +127,17 @@ Calculate x^y where x is double y is integer
 Follow the bit of y to recursively calculate.
 */
 
-double expoRecursion(double x, int y){
+double expoRecursion(double x, int y) {
 
 	/* Consider that y could be negative number*/
-	if( y < 0) {
+	if ( y < 0) {
 		y = - y;
 		x = 1.0 / x;
 	}
-	if (y && (y % 2)){
+	if (y && (y % 2)) {
 		return x * expoRecursion(x, y - 1);
-	} 
-	if(y && !(y % 2)){
+	}
+	if (y && !(y % 2)) {
 		return expoRecursion(x, y >> 1) * expoRecursion(x, y >> 1);
 	}
 	return 1;
@@ -148,30 +148,30 @@ double expoRecursion(double x, int y){
 Now consider loop version for problem
 */
 
-double expoLoop(double x, int y){
+double expoLoop(double x, int y) {
 
-	if(y < 0){
+	if (y < 0) {
 		y = -y;
 		x = 1.0 / x;
 	}
 
 	double ans = 1.0;
 
-	while (y){
-		/* 
+	while (y) {
+		/*
 		if the most right is set bit then multiply
 		current x value once more */
-		if( y & 0x1){
+		if ( y & 0x1) {
 			ans *= x;
 		}
 
-		/* 
+		/*
 		Each time y = y / (2 ^ n) ->
 		x = x ^ [y  * (2 ^ n)]
 		looping until y = 1
 		*/
 		x *= x;
-		y = y >> 1;	
+		y = y >> 1;
 	}
 	return ans;
 }
@@ -184,7 +184,7 @@ Reverse the input integer
 Brute-force: loop with 10X for each digit
 */
 
-int revIntBruteForce(int n){
+int revIntBruteForce(int n) {
 
 	int numPos = 1;
 	int prePos = 1;
@@ -194,7 +194,7 @@ int revIntBruteForce(int n){
 
 	n = negative ? -n : n;
 
-	while(numPos <= n){
+	while (numPos <= n) {
 		ans *= 10;
 		numPos *= 10;
 		prePos = numPos / 10;
@@ -207,14 +207,14 @@ int revIntBruteForce(int n){
 A better approch
 */
 
-int revIntImprove(int n){
+int revIntImprove(int n) {
 
 	int ans = 0;
 	bool negative = n < 0;
 
 	n = negative ? -n : n;
 
-	while(n){
+	while (n) {
 		ans = ans * 10 + n % 10;
 		n /= 10;
 	}
@@ -227,17 +227,17 @@ int revIntImprove(int n){
 Brute force through the way in 5.2
 */
 
-unsigned long revBits(unsigned long num){
+unsigned long revBits(unsigned long num) {
 
 	int len = (sizeof num) * 4;
 	std::cout << len << std::endl;
 
-	for(int i = 0; i < len / 2; i++){
-		if (((num >> i) & 0x1) != ((num >> (len - i - 1)) & 0x1)){
+	for (int i = 0; i < len / 2; i++) {
+		if (((num >> i) & 0x1) != ((num >> (len - i - 1)) & 0x1)) {
 
 			unsigned long mask = (1L << i) | (1L << (len - i - 1));
 			num ^= mask;
-		}	
+		}
 	}
 	return num;
 }
@@ -248,7 +248,7 @@ Another better way, similar as problem 5.2, we could use
 a table to save the reverse like 16 bits as the key, and
 the responding value is it's reverse.
 
-The running time complexity would be O(n/L) where L is 
+The running time complexity would be O(n/L) where L is
 the length of the key.
 
 The space complexity would be O(L * 2^(L + 1)) as worst case
@@ -265,22 +265,22 @@ It may have same time complexsity of worst case as optimal
 method, but the average time complexsity is not optimal
 */
 
-int checkPalindrome(long num){
+int checkPalindrome(long num) {
 
-	if(num < 0) return 0;
+	if (num < 0) return 0;
 
 	long rev = 0;
 	long orig = num;
 
-	while(num){
+	while (num) {
 		rev = rev * 10 + num % 10;
 		num /= 10;
 	}
 	return rev == orig ? 1 : 0;
 }
 
-/* 
-There is a better way. 
+/*
+There is a better way.
 
 When check failed, stop running immediately.
 
@@ -288,18 +288,18 @@ Note: the length of most significant digit would
 be given by floor(log10(X)) + 1.
 */
 
-int checkPalindromeImprove(long num){
+int checkPalindromeImprove(long num) {
 
-	if(num < 0) return 0;
+	if (num < 0) return 0;
 
 	int maskLength = floor(log10(num)) + 1;
 	int numMask = pow(10, maskLength - 1);
 
-	while(num){
+	while (num) {
 		int tail = num % 10;
 		int head =  num / numMask;
 
-		if(tail != head) return 0;
+		if (tail != head) return 0;
 
 		/* Remove head and tail for parameter */
 		num %= numMask;
@@ -309,61 +309,61 @@ int checkPalindromeImprove(long num){
 
 	return 1;
 
-} 
+}
 
 /******* 5.11 Rectangle Intersection Problem *******/
 
 /*
 If the rectrangle is formed by lowest width and height,
 and we has width and height as well. We have O(1) time to check
-if two rectangle has intersection and what the rectangle that 
+if two rectangle has intersection and what the rectangle that
 such intersection formed.
 */
 
 typedef struct rec {
 	int x, y, width, height;
 
-	rec(int x, int y, int width, int height): x(x),y(y),width(width),height(height){
+	rec(int x, int y, int width, int height): x(x), y(y), width(width), height(height) {
 		/*std::cout << "rectangle created" << std::endl;*/
 	}
 } Rectangle;
 
-int isIntersection(Rectangle & R1, Rectangle & R2){
+int isIntersection(Rectangle & R1, Rectangle & R2) {
 	return R1.x <= R2.x + R2.x + R2.width &&
-		   R1.y <= R2.y + R2.y + R2.height &&
-		   R2.x <= R1.x + R1.x + R1.width &&
-		   R2.y <= R2.y + R2.y + R2.height;
+	       R1.y <= R2.y + R2.y + R2.height &&
+	       R2.x <= R1.x + R1.x + R1.width &&
+	       R2.y <= R2.y + R2.y + R2.height;
 }
 
-Rectangle intersectionRec(Rectangle &R1, Rectangle &R2){
+Rectangle intersectionRec(Rectangle &R1, Rectangle &R2) {
 
-	if(isIntersection(R1, R2)){
+	if (isIntersection(R1, R2)) {
 		Rectangle result = Rectangle(
-			std::max(R1.x, R2.x),
-			std::max(R1.y, R2.y),
-			std::min(R1.x + R1.width, R2.x + R2.width) - std::max(R1.x,R2.x),
-			std::min(R1.y + R1.height, R2.y + R2.height) - std::max(R1.y,R2.y)
-		);
+		                       std::max(R1.x, R2.x),
+		                       std::max(R1.y, R2.y),
+		                       std::min(R1.x + R1.width, R2.x + R2.width) - std::max(R1.x, R2.x),
+		                       std::min(R1.y + R1.height, R2.y + R2.height) - std::max(R1.y, R2.y)
+		                   );
 
 		return result;
 	}
 
-	return Rectangle(0,0,-1,-1);
+	return Rectangle(0, 0, -1, -1);
 }
 
-void unitTestForRecItersec(void){
+void unitTestForRecItersec(void) {
 
-	Rectangle R1 = Rectangle(0,0,1,1);
-	Rectangle R2 = Rectangle(1,1,2,3);
-	Rectangle R3 = Rectangle(2,0,1,3);
+	Rectangle R1 = Rectangle(0, 0, 1, 1);
+	Rectangle R2 = Rectangle(1, 1, 2, 3);
+	Rectangle R3 = Rectangle(2, 0, 1, 3);
 
 	Rectangle res1 = intersectionRec(R1, R2); // one pix intersection
 	Rectangle res2 = intersectionRec(R2, R3); // rectangle intersection
 	Rectangle res3 = intersectionRec(R1, R3); // no intersection
 
-	std::cout << "R1 & R2: " <<res1.x << "," << res1.y << "," << res1.width << "," << res1.height << std::endl;
-	std::cout << "R2 & R3: " <<res2.x << "," << res2.y << "," << res2.width << "," << res2.height << std::endl;
-	std::cout << "R1 & R3: " <<res3.x << "," << res3.y << "," << res3.width << "," << res3.height << std::endl;
+	std::cout << "R1 & R2: " << res1.x << "," << res1.y << "," << res1.width << "," << res1.height << std::endl;
+	std::cout << "R2 & R3: " << res2.x << "," << res2.y << "," << res2.width << "," << res2.height << std::endl;
+	std::cout << "R1 & R3: " << res3.x << "," << res3.y << "," << res3.width << "," << res3.height << std::endl;
 
 }
 
@@ -373,8 +373,8 @@ void unitTestForRecItersec(void){
 1.	Even tough it is easy to use O(n) space for
 	arry related quesiotn, it is better to solve
 	it without alloctating addtional storage.
-	
-2.	Take advantage for the face that you can operate 
+
+2.	Take advantage for the face that you can operate
 	efficiently on both ends.
 
 3.  When operating 2D array, use parallel logic for rows
@@ -384,7 +384,7 @@ void unitTestForRecItersec(void){
 
 /***** <vector> *****
 Dynamic size, stored in heap.
-    
+
 
 *********************
 
@@ -396,11 +396,11 @@ Dynamic size, stored in heap.
 **** <alogrithm> ****
 1. std::binary_search(A.begin(), A.end(), val, comp)
 
-->	std::sort(A.begin(), A.end(), comp) to sort the 
+->	std::sort(A.begin(), A.end(), comp) to sort the
 	container first
 
 -> 	comp is function that compares two elements, and
-	return if the first one is 'lower' than the second one  
+	return if the first one is 'lower' than the second one
 -> 	Return true if find val
 
 -> 	Return false if not find val
@@ -414,13 +414,13 @@ Dynamic size, stored in heap.
 2. std::lower_bound(A.begin(), A.end(), val, comp)
    std::upper_bound(A.begin(), A.end(), val, comp)
 
-->	std::sort(A.begin(), A.end(), comp) to sort the 
+->	std::sort(A.begin(), A.end(), comp) to sort the
 	container first
 
 -> 	comp is function that compares two elements, and
-	return if the first one is 'lower' than the second one 
+	return if the first one is 'lower' than the second one
 
-->	Used if there are multiple elements in the range whose 
+->	Used if there are multiple elements in the range whose
 	value equals the value val.
 
 ->	a. for lower_bound, it returns the first iterator position
@@ -428,7 +428,7 @@ Dynamic size, stored in heap.
 
 	b. for upper_bound, it returns the first iterator position
 	such that the values is larger than val. i.e. *it > val
-	
+
 	else return the A.end()
 
 ->	Performs at most log2(N) + 1
@@ -457,9 +457,9 @@ Dynamic size, stored in heap.
 
 	using std::swap;
 	swap(x, y);
-	
+
 	instead of std::swap(x,y) because the approach outlined
-	about ADL(Argument-dependent lookup) can find a better 
+	about ADL(Argument-dependent lookup) can find a better
 	version.
 
 5. min_element(A.begin(), A.end(), comp)
@@ -472,7 +472,7 @@ Dynamic size, stored in heap.
 
 ->	min_element return the iterator position of minimum
 	val in the range of [A.begin, A.end)
-	
+
 	max_element return the iterator position of maximum
 	val in the range of [A.begin, A.end)
 
@@ -492,11 +492,11 @@ Dynamic size, stored in heap.
 
 ->	up to linear the distance (first, and last)
 
-->	Here is a insert sort implementation by using 
+->	Here is a insert sort implementation by using
 	std::upper_bound and std::rotate
 */
-void insert_sort(std::vector<int> & v){
-	for (auto i = v.begin(); i != v.end(); i++){
+void insert_sort(std::vector<int> & v) {
+	for (auto i = v.begin(); i != v.end(); i++) {
 		std::rotate(std::upper_bound(v.begin(), i, *i), i, i + 1);
 	}
 }
@@ -515,7 +515,7 @@ void insert_sort(std::vector<int> & v){
 
 	It mainly use quick-sort of median of three values,
 	which is choosing the median of [first...middle...last]
-	as the pivot. 
+	as the pivot.
 	The complexity of quick-sort could be O(N^2)
 
 	So, once the recursion times for quick-sort are larger
@@ -524,9 +524,9 @@ void insert_sort(std::vector<int> & v){
 	O(NlogN)
 
 	O(N + (N - 1) + ... + (N - log(N))) < O(NlogN) complexity for quick-sort
-	O((N - log(N)) log(N - log(N))) < O(NlogN) for heap-sort 
+	O((N - log(N)) log(N - log(N))) < O(NlogN) for heap-sort
 
-	Thus, the overall complexity is less than 2 * O(NlogN) for the 
+	Thus, the overall complexity is less than 2 * O(NlogN) for the
 	std::sort()
 
 ->	comp is function that compares two elements, and
@@ -534,12 +534,12 @@ void insert_sort(std::vector<int> & v){
 
 	This comp could be a lambda, for example:
 */
-void lambda_sort(std::vector<int> &v){
+void lambda_sort(std::vector<int> &v) {
 
-	return std::sort(v.begin(), v.end(), [](const int& a, const int& b){
+	return std::sort(v.begin(), v.end(), [](const int& a, const int& b) {
 		return b < a;
 	});
-} 
+}
 /*
 ->	The lambda function is a c++11 feature
 
@@ -557,20 +557,20 @@ all the element that is larger than A[i] right
 use loop two pinters
 */
 
-void rearrange (std::vector<int> & v, int idx){
+void rearrange (std::vector<int> & v, int idx) {
 
 	int smaller = 0, unclassified = 0, larger = v.size() - 1;
 	int pivot = v[idx];
 
-	while (unclassified < larger){
+	while (unclassified < larger) {
 
 		// Basic idea : smaller++ when smaller is smaller than pivot
 		//				larger-- when larger is larger than pivot
 		//				unclassified go through from left to right
 
-		if(v[unclassified] < pivot){
+		if (v[unclassified] < pivot) {
 			std::swap(v[smaller++], v[unclassified++]);
-		} else if(v[unclassified] == pivot){
+		} else if (v[unclassified] == pivot) {
 			unclassified++;
 		} else {
 			std::swap(v[unclassified], v[larger--]);
@@ -578,17 +578,84 @@ void rearrange (std::vector<int> & v, int idx){
 	}
 }
 
+/******* 6.2 Increment an arbitrary-preciion Integer *******/
+
+/*
+<1,2,9> -> <1,3,0> [Easy]: O(N)
+*/
+
+void incre_arb_int(std::vector<int> & v) {
+
+	for (auto it = v.rbegin(); it != v.rend(); ++it) {
+
+		if ( *it != 9) {
+
+			*it += 1;
+			break;
+
+		} else if (it == v.rend() - 1) {
+			*it = 0;
+			v.insert(v.begin(), 1);
+		} else {
+			*it = 0;
+		}
+	}
+}
+
+/******* 6.3 Multiply two arbitrary-preciion Integers *******/
+
+/*
+<-1,2> * <1,2> -> <-1,4,4>
+
+** sometimes number iterator is better than pointer iterator***
+
+->	using std::deque to remove leading zero convenient. However
+	Elements in a deque are not contiguous in memory, and 
+	vector elements are guaranteed to be.
+*/
+
+std::deque<int> mult_arb_int (std::vector<int> v1, std::vector<int> v2){
+
+	std::deque<int> res(v1.size() + v2.size(), 0);
+
+	int sign = v1[0] * v2[0] < 0 ? -1 : 1; // negative
+
+	v1[0] = abs(v1[0]);
+	v2[0] = abs(v2[0]);
+
+	for(int i = v1.size() - 1; i >= 0; --i){
+
+		for(int j = v2.size() - 1; j >= 0; --j){
+
+			res[i + j + 1] += v1[i] * v2[j];
+			res[i + j] += res[i + j + 1] / 10;
+			res[i + j + 1] = res[i + j + 1] % 10;
+		}
+	}
+
+	for (auto i = res.begin(); *i == 0; ++i){
+		res.pop_front();
+	}
+
+	res[0] = res[0] * sign;
+
+	return res;
+}
+
+/******* 6.4 Advancing through an array *******/
 
 
-int main(){
+int main() {
 
-	int array[] = {3,1,2,5,4,0};
+	int array1[] = {1,9,3,7,0,7,7,2,1};
+	int array2[] = {-7,6,1,8,3,8,2,5,7,2,8,7};
 
-	std::vector<int> v(array, array + 6);
+	std::vector<int> v1(array1, array1 + 9);
+	std::vector<int> v2(array2, array2 + 12);
 
-	rearrange(v, 0);
+	
 
-	std::cout << v << std::endl;
+	std::cout << mult_arb_int(v1,v2) << std::endl;
 
 	return 0;
 }
--- EPI.h
+++ EPI.h
@@ -2,6 +2,7 @@
 #define EPI_H
 
 #include <iostream>
+#include <stdlib.h>
 #include <string>
 
 #include <bitset>
@@ -9,19 +10,30 @@
 #include <limits>
 #include <math.h>
 #include <vector>
+#include <deque>
 #include <iterator>
 
 #include <algorithm>
 
 
-// Overload << to cout elements in vector easily for testing 
+// Overload << to cout elements in vector easily for testing
 template<class T>
 std::ostream& operator<<(std::ostream& stream, const std::vector<T>& values)
 {
-    stream << "[ ";
-    copy( begin(values), end(values), std::ostream_iterator<T>(stream, " ") );
-    stream << ']';
-    return stream;
+	stream << "[ ";
+	copy( begin(values), end(values), std::ostream_iterator<T>(stream, " ") );
+	stream << ',';
+	stream << ']';
+	return stream;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& stream, const std::deque<T>& values)
+{
+	stream << "[ ";
+	copy( begin(values), end(values), std::ostream_iterator<T>(stream, " ") );
+	stream << ']';
+	return stream;
 }
 
 #endif
